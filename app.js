61f3f6e17242feaed6add2d0aeccdd93c14ecbda
S-Gyr00v <94624698+S-Gyr00v@users.noreply.github.com>
2022-07-03 19:37:39 +0300

    commit 6

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const app = express();
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(express.json());
-
 app.use((req, res, next) => {
   req.user = {
     _id: '62c19a77351acfeb4979917a' // вставьте сюда _id созданного в предыдущем пункте пользователя
--- controllers/users.js
+++ controllers/users.js
@@ -40,7 +40,7 @@ const updateUser = (req, res) => {
   User.findByIdAndUpdate(
     req.user._id,
     { name, about },
-    { new: true,},
+    { new: true, runValidators: true},
   ).then((user) => {
     if (!user) {
       res.status(404).send({ message: 'Пользователь с указанным _id не найден. ' });
