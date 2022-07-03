2fb1571af9f92599c0ef7ed563d67ee4bc4050b5
S-Gyr00v <94624698+S-Gyr00v@users.noreply.github.com>
2022-07-03 19:07:59 +0300

    commit 5

--- routes/cards.js
+++ routes/cards.js
@@ -4,9 +4,13 @@ const {
   getCards,
   createCard,
   deleteCard,
+  likeCard,
+  unLikeCard,
 } = require('../controllers/cards');
 
 router.get('/', getCards);
 router.post('/', createCard);
 router.delete('/:cardId', deleteCard);
+router.put('/:cardId/likes', likeCard);
+router.delete('/:cardId/likes',unLikeCard);
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,13 @@ const {
   getUsers,
   getUsersId,
   createUser,
+  updateUser,
+  updateAvatar
 } = require('../controllers/users');
 
 router.get('/', getUsers);
 router.get('/:userId', getUsersId);
 router.post('/', createUser);
+router.patch('/me', updateUser);
+router.patch('/me/avatar', updateAvatar);
 module.exports = router;
